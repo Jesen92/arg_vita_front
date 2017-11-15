e2f3489e7f1ee833e282d8a15b0c45374cbb9f00
Jesen92 <hjesenovic@gmail.com>
2017-11-15 14:39:05 +0100

    assets

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -103,4 +103,6 @@ Rails.application.configure do
   config.action_mailer.perform_deliveries = true
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.delivery_method = :smtp
+
+  config.serve_static_assets = true
 end
