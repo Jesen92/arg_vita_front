517eb182c031e7d34824a5eff2b4bfbdebf0b060
Jesen92 <hjesenovic@gmail.com>
2018-01-25 11:28:42 +0100

    assets precompile

---
+++ public/assets/select_category-481c5da903d5ac318217f4e77738952001fa94364c49618c0aa539261b8d8e1d.js
@@ -0,0 +1,26 @@
+(function() {
+  $(function() {
+    var categories, material, options;
+    categories = $('#filterrific_with_ssubcategory_id').html();
+    material = $('#filterrific_with_subcategory_id :selected').text();
+    if (material === "- Sve -") {
+      $('#filterrific_with_ssubcategory_id').empty();
+      $('#filterrific_with_ssubcategory_id').trigger("chosen:updated");
+    } else {
+      options = $(categories).filter("optgroup[label='" + material + "']").prepend($("<option></option>").attr("value", 9999).text("- Sve -")).html();
+      $('#filterrific_with_ssubcategory_id').html(options);
+      $('#filterrific_with_ssubcategory_id').trigger("chosen:updated");
+    }
+    return $('#filterrific_with_subcategory_id').change(function() {
+      material = $('#filterrific_with_subcategory_id :selected').text();
+      options = $(categories).filter("optgroup[label='" + material + "']").prepend($("<option></option>").attr("value", 9999).text("- Sve -"));
+      if (options) {
+        $('#filterrific_with_ssubcategory_id').html(options);
+        return $('#filterrific_with_ssubcategory_id').parent().show();
+      } else {
+        return $('#filterrific_with_ssubcategory_id').trigger("chosen:updated").empty();
+      }
+    });
+  });
+
+}).call(this);
