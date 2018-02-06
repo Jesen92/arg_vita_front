1f34d930f3414d68bf44d72782ccba43db27464d
Jesen92 <hjesenovic@gmail.com>
2018-02-06 14:15:17 +0100

    mailgun -prod

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -107,7 +107,7 @@ Rails.application.configure do
       :address        => ENV['MAILGUN_SMTP_SERVER'],
       :user_name      => ENV['MAILGUN_SMTP_LOGIN'],
       :password       => ENV['MAILGUN_SMTP_PASSWORD'],
-      :domain         => 'http://argentumvita-front.herokuapp.com/', #eg: 'yourappname.herokuapp.com'
+      :domain         => 'https://argentumvita-front.herokuapp.com/', #eg: 'yourappname.herokuapp.com'
       :authentication => :plain,
   }
 
